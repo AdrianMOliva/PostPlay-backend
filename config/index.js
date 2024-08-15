0a676ef0cfc713947b83a64abd9146f719b1e5ed
Adrian Martin <martinolivaadrian@gmail.com>
2024-08-15 18:57:56 +0200

    changed something in cors

--- config/index.js
+++ config/index.js
@@ -22,19 +22,10 @@ module.exports = (app) => {
   app.set("trust proxy", 1);
 
   // controls a very specific header to pass headers from the frontend
-  const whitelist = [FRONTEND_URL, "http://localhost:5173"];
 
   app.use(
     cors({
-      origin: (origin, callback) => {
-        // Allow requests with no origin like mobile apps or curl commands
-        if (!origin) return callback(null, true);
-        if (whitelist.indexOf(origin) !== -1) {
-          callback(null, true);
-        } else {
-          callback(new Error("Not allowed by CORS"));
-        }
-      },
+      origin: [FRONTEND_URL],
     })
   );
 
